0d292028ffa87abd972041dc713eb859ad8a0ad3
willstay <willstay@mail.ru>
2018-03-22 17:12:48 +0300

    add some streams

--- javacore-song-task/src/main/java/ru/liga/songtask/analyzer/NotesAnalyzer.java
+++ javacore-song-task/src/main/java/ru/liga/songtask/analyzer/NotesAnalyzer.java
@@ -3,7 +3,6 @@ package ru.liga.songtask.analyzer;
 import ru.liga.songtask.domain.Note;
 
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class NotesAnalyzer {
@@ -52,39 +51,21 @@ public class NotesAnalyzer {
     public Map<Note, Integer> analyzeNotesHeight() {
         List<Note> notesListCopy = new ArrayList<>(notesList);
         Map<Note, Integer> notesHeightMap = new TreeMap<>(NOTE_COMPARATOR_FREQUENCY.reversed());
-
         notesListCopy.stream()
-                .forEach(note -> {
-                    if (!notesHeightMap.containsKey(note)) {
-                        notesHeightMap.put(note, 1);
-                    } else {
-                        notesHeightMap.put(note, notesHeightMap.get(note) + 1);
-                    }
-                });
+                .forEach(note -> notesHeightMap.merge(note,1,(oldValue, newValue) -> oldValue + newValue));
         return notesHeightMap;
     }
 
     public Map<Integer, Integer> analyzeIntervals() {
         List<Note> notesListCopy = new ArrayList<>(notesList);
         List<Integer> intervalList = new ArrayList<>();
-
-//        notesListCopy.stream()
-//                .map(note -> note.sign())
-
         for (int i = 0; i < notesListCopy.size() - 1; i++) {
             intervalList.add(notesListCopy.get(i).sign().diffInSemitones(
                     notesListCopy.get(i + 1).sign()));
         }
-
         Map<Integer, Integer> intervalMap = new TreeMap<>();
-
-        for (Integer interval : intervalList) {
-            if (!intervalMap.containsKey(interval)) {
-                intervalMap.put(interval, 1);
-            } else {
-                intervalMap.put(interval, intervalMap.get(interval) + 1);
-            }
-        }
+        intervalList.stream()
+                .forEach(interval -> intervalMap.merge(interval, 1, (oldValue, newValue) -> oldValue + newValue));
 
         return intervalMap;
     }
